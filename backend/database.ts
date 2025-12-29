88535686bf7301cd34409e997a5f88891cf30649
Dmitry Karyakin <skycrazyk@gmail.com>
2025-12-29 16:53:34 +0500

    Реализовано каскадное удаление сущностей

--- backend/database.ts
+++ backend/database.ts
@@ -4,37 +4,36 @@ export const db = new Database('database.db')
 
 // TODO Выполнять запросы через exec
 // TODO Разобраться с миграциями
-db.sql`
+db.exec(`
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email TEXT NOT NULL UNIQUE
 );
-`
 
-db.sql`
 CREATE TABLE IF NOT EXISTS users_identities (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER REFERENCES users(id) NOT NULL,
+    user_id INTEGER NOT NULL,
     iss VARCHAR(500) NOT NULL,
     sub VARCHAR(500) NOT NULL,
     UNIQUE(iss, sub),
-    UNIQUE(user_id, iss)
+    UNIQUE(user_id, iss),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE 
 );
-`
 
-db.sql`
 CREATE TABLE IF NOT EXISTS lists (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
-    user_id INTEGER REFERENCES users(id) NOT NULL
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-`
 
-db.sql`
 CREATE TABLE IF NOT EXISTS todos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     done BOOLEAN DEFAULT FALSE NOT NULL,
-    list_id INTEGER REFERENCES lists(id) NOT NULL
+    list_id INTEGER NOT NULL,
+    FOREIGN KEY (list_id) REFERENCES lists(id) ON DELETE CASCADE
 );
-`
+`)
