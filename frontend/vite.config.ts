85380bd72c9d4d87a4c0769e8acc929b82bf06e3
Dmitry Karyakin <skycrazyk@gmail.com>
2026-01-19 08:28:25 +0200

    Убран vite-plugin-checker так как не работает в deno

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import checker from 'vite-plugin-checker'
 import deno from '@deno/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    deno(),
-    react(),
-    checker({
-      // TODO это не работает
-      typescript: true
-    })
-  ]
+  plugins: [deno(), react()]
 })
